a44a43ae0c1f504dddf2ebbe4c89ad9b8832303a
Anthony Ringoet <anthonyringoet@gmail.com>
2013-03-12 15:12:53 +0100

    update docco export, add readme

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,15 @@
 module.exports = function(grunt){
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    // set up first custom task: concat
+    // the options depend on the task you want to use
     concat: {
       options: {
         // string to put between concatenated files
         separator: ';'
       },
       dist: {
-        // files to concat
+        // files to concat together
         src: ['js/*.js'],
         // location of result file
         dest: 'jsmin/<%= pkg.name %>.js'
@@ -15,7 +17,9 @@ module.exports = function(grunt){
     }
   });
 
+  // load contrib task files
+  // note: these should be installed from npm
   grunt.loadNpmTasks('grunt-contrib-concat');
-
+  // register what to do when using the default 'grunt' command
   grunt.registerTask('default', ['concat']);
 };
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Grunt demo project
+
+This is a demo setup to show the basic workings of [Grunt](http://gruntjs.com). There's a complementary blogpost on [Knife.io](http://knife.io)
